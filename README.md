4a346f6a392ac5227107d853e209631c2503f000
Ole Petter B. Stokke <doktoresel@gmail.com>
2018-03-06 21:22:36 +0100

    most functionality in place

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# treehouse-project7
-Build a Twitter Interface, using Node and Express.
+Treehouse Fullstack Javascript Techdegree, 
+project #7: 
+# "Build a Twitter Interface"
+
+by Ole Petter Baugerød Stokke
+www.olepetterstokke.no/treehouse/project7
+
+Express app using the Twitter’s REST API
+to display the 5 last tweets, followers, 
+and incoming messages from Twitter. Also 
+let's you tweet. 
--- app.js
+++ app.js
@@ -1,43 +1,179 @@
-//setting up express
+/*****************************************
+Treehouse Fullstack Javascript Techdegree,
+project #7: "Build a Twitter Interface"
+by Ole Petter Baugerød Stokke
+www.olepetterstokke.no/treehouse/project7
+******************************************
+Express app using the Twitter’s REST API
+to display the 5 last tweets, followers, 
+and incoming messages from Twitter. Also 
+let's you tweet. 
+*****************************************/
+
+/*****************************************
+SETUP
+getting all we need set up
+*****************************************/
+
+//set up express
 const express = require('express');
 const app = express(); 
 
-//setting up Twit, using config.js for credencials
+//set up Twit (using config.js for credencials)
 const Twit = require('twit');
 const twitConfig = require('./config.js');
 const T = new Twit(twitConfig.config);
-const count = 5; //number of items to get for each category
 
-//setting up static data (css, images)
+//set up static data (css, images)
 app.use('/static', express.static('public'));
 
-//setting up the pug view engine
+//set up the pug view engine (using default \views-path)
 app.set('view engine', 'pug');
 
-//getting twitter data
-app.use((res, req, next) => {
-    T.get('statuses/user_timeline', { 'count':count }, (err, data, response) => {
-        console.log(data[0].text);
+//set up bodyparser (to get text from form)
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set up empty object to contain all info needed from Twitter
+var twitterContainer = {};
+
+//set up moment for timestamps
+var moment = require('moment');
+
+/*****************************************
+MIDDLEWARE
+filling our Twitter-object with data from the API
+*****************************************/
+
+//get messages
+app.use((req, res, next) => {
+    T.get('direct_messages', { 'count': 5 }, (err, data, response) => {        
+        if (!err) {
+            twitterContainer.messages = data;
+            next();
+        } else {
+            console.log('Messages failed: ' + err.message);
+            next(err);
+        }
     });
-    next();
 });
 
-//root route render
+//get friends
+app.use((req, res, next) => {
+    T.get('friends/list', { 'count': 5 }, (err, data, response) => {
+        if (!err) {
+            twitterContainer.friends = data.users;
+            next();
+        } else {
+            console.log('Friends failed: ' + err.message);
+            next(err);
+        }
+    });
+});
+
+//get timeline
+app.use((req, res, next) => {
+    T.get('statuses/user_timeline', { 'count': 5 }, (err, data, response) => {
+        if (!err) {
+            twitterContainer.tweets = data;
+
+            //change timestamp of each tweet, using moment
+            twitterContainer.tweets.forEach(tweet => {
+                tweet.created_at = moment(tweet.created_at).fromNow();
+            });
+
+            next();
+            
+        } else {
+            console.log('Timeline failed: ' + err.message);
+            next(err);
+        }
+    });
+});
+
+//ROUTES
+//rendering root, posting tweets
+
+//get request at root
 app.get('/', (req, res) => {
-    res.render('index');
+
+    //render index with Twitter-data from twitterContainer
+    res.render('index', {
+        tweets: twitterContainer.tweets,
+        friends: twitterContainer.friends,
+        messages: twitterContainer.messages
+    });
 });
 
-// Each rendered result must include all of the information seen in the sample layout:
-// *tweets -message content -# of retweets -# of likes -date tweeted
-// *friends -profile image -real name -screenname
-// *messages -message body -date the message was sent -time the message was sent
+//post request at root - triggered from form, to post a tweet
+app.post('/', (req, res) => {
 
-// Your 5 most recent tweets.
-// Your 5 most recent friends.
-// Your 5 most recent private messages.
+    //tweet text from the tweet-textbox using bodyparser
+    const tweet = req.body.tweet;
 
+    T.post('statuses/update', {
+        status: tweet
+    });
 
-//listen for incoming traffic
-app.listen(3000, () => {
-    console.log('Twitter Interface is listening.');
+    res.redirect('/'); //render site again to show new tweet
 });
+
+//routes for debug purposes
+app.get('/json/timeline', (req, res) => {
+    T.get('statuses/user_timeline', { 'count': 5 }, (err, data, response) => {        
+        res.send(data);
+    });
+});
+
+app.get('/json/messages', (req, res) => {
+    T.get('direct_messages', { 'count': 5 }, (err, data, response) => {        
+        res.send(data);
+    });
+});
+
+app.get('/json/friends', (req, res) => {
+    T.get('friends/list', { 'count': 5 }, (err, data, response) => {        
+        res.send(data);
+    });
+});
+
+app.get('/json/account', (req, res) => {
+    T.get('account/verify_credentials', (err, data, response) => {        
+        res.send(data);
+    });
+});
+
+//////////////////
+//ERROR HANDLING//
+//////////////////
+
+//404 error
+app.use((req, res, next) => {
+    const err = new Error ('404: Page not found.');
+    next(err);
+});
+
+//render error page
+app.use((err, req, res, next) => {
+    res.render('error', { error : err }); 
+});
+
+////////////////
+//SERVER SETUP//
+////////////////
+
+//listen for incoming traffic at port 3000, or env.PORT if deployed
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Twitter Client is listening...');
+});
+
+/* 
+TODO:
+* ordne tidsgreia på tweets
+* se over hva slags annen dummydata som ligger der
+X fikse layouten
+* sjekke om jeg bruker puggene riktig
+
+moment
+http://momentjs.com
+*/
